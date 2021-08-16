d3709d277ad2fbe18a63a078486aa6e8c3574d3d
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-08-16 16:58:33 +0700

    second commit

--- test/NextDayCalculatorTest.java
+++ test/NextDayCalculatorTest.java
@@ -7,46 +7,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class NextDayCalculatorTest {
     @Test
     public void testNextDayOfDay1Month1() {
-        LocalDate date = LocalDate.parse("2021-01-01");
+        String date = "2021-01-01";
         LocalDate actual = LocalDate.parse("2021-01-02");
 
-        LocalDate nextDay = NextDayCalculator.nextDay("2021-01-01");
+        LocalDate nextDay = NextDayCalculator.nextDay(date);
         assertEquals(actual, nextDay);
     }
 
     @Test
     public void testNextDayOfDay28Month2LeapYear() {
-        LocalDate date = LocalDate.parse("2020-02-28");
+        String date = "2020-02-28";
         LocalDate actual = LocalDate.parse("2020-02-29");
 
-        LocalDate nextDay = NextDayCalculator.nextDay("2020-02-28");
+        LocalDate nextDay = NextDayCalculator.nextDay(date);
         assertEquals(actual, nextDay);
     }
 
     @Test
     public void testNextDayOfDay28Month2() {
-        LocalDate date = LocalDate.parse("2021-02-28");
+        String date = "2021-02-28";
         LocalDate actual = LocalDate.parse("2021-03-01");
 
-        LocalDate nextDay = NextDayCalculator.nextDay("2021-02-28");
+        LocalDate nextDay = NextDayCalculator.nextDay(date);
         assertEquals(actual, nextDay);
     }
 
     @Test
     public void testNextDayOfDay31Month12() {
-        LocalDate date = LocalDate.parse("2021-12-31");
+        String date = "2021-12-31";
         LocalDate actual = LocalDate.parse("2022-01-01");
 
-        LocalDate nextDay = NextDayCalculator.nextDay("2021-12-31");
+        LocalDate nextDay = NextDayCalculator.nextDay(date);
         assertEquals(actual, nextDay);
     }
 
     @Test
     public void testNextDayOfDay30Month4() {
-        LocalDate date = LocalDate.parse("2021-04-30");
+        String date = "2021-04-30";
         LocalDate actual = LocalDate.parse("2021-05-01");
 
-        LocalDate nextDay = NextDayCalculator.nextDay("2021-04-30");
+        LocalDate nextDay = NextDayCalculator.nextDay(date);
         assertEquals(actual, nextDay);
     }
 }
